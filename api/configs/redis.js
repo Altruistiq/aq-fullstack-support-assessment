2a8bd8dd31c4b5feb5075ec7556d3253217a9a98
satyavh <satya.vh@gmail.com>
2025-02-06 12:10:35 +0100

    chore: first commit

---
+++ api/configs/redis.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+// Create a new Redis client instance
+const redisClient = createClient();
+
+// Handle Redis client errors
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+// Function to connect to the Redis server
+const connectRedis = async () => {
+  await redisClient.connect();
+  await redisClient.FLUSHDB();
+};
+
+// Connect to Redis server immediately upon loading the module
+connectRedis();
+
+// Export the Redis client instance for use in other parts of the application
+export default redisClient;
